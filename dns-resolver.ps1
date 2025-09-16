fd6d8dadff5036558d6c98f68792fb30a9d2c911
Michaelfoop <michaelrtmehl@gmail.com>
2025-09-15 23:17:05 -0400

    For 3.1

---
+++ dns-resolver.ps1
@@ -0,0 +1,9 @@
+﻿param($network, $server)
+
+for ($i=1;$i -lt 255;$i++){
+    $ip = ($network + "." + $i)
+    $hn = (Resolve-DnsName -DnsOnly $ip -Server $server -ErrorAction Ignore)
+    if ($null -ne $hn) {
+        Write-Host $ip $hn.NameHost
+    }
+}
